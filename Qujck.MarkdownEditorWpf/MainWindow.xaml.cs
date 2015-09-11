6bc119b89caeca9f06a1aba593cb94a47207a7da
Peter Parker <social@qujck.com>
2015-09-11 21:38:06 +0100

    Capture mousewheel event and bubble to parent that can handle it

--- Qujck.MarkdownEditorWpf/MainWindow.xaml.cs
+++ Qujck.MarkdownEditorWpf/MainWindow.xaml.cs
@@ -35,6 +35,19 @@ namespace Qujck.MarkdownEditor
         {
             InitializeComponent();
 
+            this.TextEditor.TextArea.MouseWheel += (sender, e) =>
+                {
+                    if (!e.Handled)
+                    {
+                        e.Handled = true;
+                        var eventArg = new MouseWheelEventArgs(e.MouseDevice, e.Timestamp, e.Delta);
+                        eventArg.RoutedEvent = UIElement.MouseWheelEvent;
+                        eventArg.Source = sender;
+                        var parent = ((Control)sender).Parent as UIElement;
+                        parent.RaiseEvent(eventArg);
+                    }
+                };
+
             this.resolver = new CompositionRoot();
 
             this.InitialiseHtml();
