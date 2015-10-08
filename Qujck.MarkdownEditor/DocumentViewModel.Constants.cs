466d2486748faf1d3ce8e282ff7af9af881371a1
Peter Parker <social@qujck.com>
2015-10-08 10:08:11 +0100

    separate data from behaviour

---
+++ Qujck.MarkdownEditor/DocumentViewModel.Constants.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Qujck.MarkdownEditor.Infrastructure;
+
+namespace Qujck.MarkdownEditor
+{
+    public sealed partial class DocumentViewModel
+    {
+        const string ON = "1*";
+        const string OFF = "0";
+
+        const string LeftColumnWidth = "LeftColumnWidth";
+        const string RightColumnWidth = "RightColumnWidth";
+        const string TopRowHeight = "TopRowHeight";
+        const string BottomRowHeight = "BottomRowHeight";
+        const string TextEditorColumn = "TextEditorColumn";
+        const string TextEditorRow = "TextEditorRow";
+        const string RenderedViewColumn = "RenderedViewColumn";
+        const string RenderedViewRow = "RenderedViewRow";
+        const string CurrentView = "CurrentView";
+
+        private enum View
+        {
+            Vertical,
+            Horizontal,
+            TextEditor,
+            RenderedView
+        }
+
+        private static readonly IDictionary<string, object> VerticalView =
+            new Dictionary<string, object>
+            {
+                { LeftColumnWidth, ON },
+                { RightColumnWidth, ON },
+                { TopRowHeight, ON },
+                { BottomRowHeight, OFF },
+                { TextEditorColumn, 0 },
+                { TextEditorRow, 0 },
+                { RenderedViewColumn, 1 },
+                { RenderedViewRow, 0 },
+                { CurrentView, View.Vertical }
+            };
+
+        private static readonly IDictionary<string, object> HorizontalView =
+            new Dictionary<string, object>
+            {
+                { LeftColumnWidth, ON },
+                { RightColumnWidth, OFF },
+                { TopRowHeight, ON },
+                { BottomRowHeight, OFF },
+                { TextEditorColumn, 0 },
+                { TextEditorRow, 0 },
+                { RenderedViewColumn, 0 },
+                { RenderedViewRow, 1 },
+                { CurrentView, View.Horizontal }
+            };
+
+        private static readonly IDictionary<string, object> TextEditorView =
+            new Dictionary<string, object>
+            {
+                { LeftColumnWidth, ON },
+                { RightColumnWidth, OFF },
+                { TopRowHeight, ON },
+                { BottomRowHeight, OFF },
+                { TextEditorColumn, 0 },
+                { TextEditorRow, 0 },
+                { RenderedViewColumn, 1 },
+                { RenderedViewRow, 0 },
+                { CurrentView, View.TextEditor }
+            };
+
+        private static readonly IDictionary<string, object> RenderedViewView =
+            new Dictionary<string, object>
+            {
+                { LeftColumnWidth, OFF },
+                { RightColumnWidth, ON },
+                { TopRowHeight, ON },
+                { BottomRowHeight, OFF },
+                { TextEditorColumn, 0 },
+                { TextEditorRow, 0 },
+                { RenderedViewColumn, 1 },
+                { RenderedViewRow, 0 },
+                { CurrentView, View.RenderedView }
+            };
+    }
+}
--- Qujck.MarkdownEditor/DocumentViewModel.cs
+++ Qujck.MarkdownEditor/DocumentViewModel.cs
@@ -9,29 +9,8 @@ using Qujck.MarkdownEditor.Infrastructure;
 
 namespace Qujck.MarkdownEditor
 {
-    public sealed class DocumentViewModel : AbstractViewModel
+    public sealed partial class DocumentViewModel : AbstractViewModel
     {
-        const string ON = "1*";
-        const string OFF = "0";
-
-        const string LeftColumnWidth = "LeftColumnWidth";
-        const string RightColumnWidth = "RightColumnWidth";
-        const string TopRowHeight = "TopRowHeight";
-        const string BottomRowHeight = "BottomRowHeight";
-        const string TextEditorColumn = "TextEditorColumn";
-        const string TextEditorRow = "TextEditorRow";
-        const string RenderedViewColumn = "RenderedViewColumn";
-        const string RenderedViewRow = "RenderedViewRow";
-        const string CurrentView = "CurrentView";
-
-        private enum View
-        {
-            Vertical,
-            Horizontal,
-            TextEditor,
-            RenderedView
-        }
-
         public DocumentViewModel() : base(VerticalView)
         {
         }
@@ -77,82 +56,6 @@ namespace Qujck.MarkdownEditor
                     return RenderedViewView;
                 default:
                     throw new InvalidOperationException();
-            }
-        }
-
-        private static IDictionary<string, object> VerticalView
-        {
-            get
-            {
-                return new Dictionary<string, object>
-                {
-                    { LeftColumnWidth, ON },
-                    { RightColumnWidth, ON },
-                    { TopRowHeight, ON },
-                    { BottomRowHeight, OFF },
-                    { TextEditorColumn, 0 },
-                    { TextEditorRow, 0 },
-                    { RenderedViewColumn, 1 },
-                    { RenderedViewRow, 0 },
-                    { CurrentView, View.Vertical }
-                };
-            }
-        }
-
-        private static IDictionary<string, object> HorizontalView
-        {
-            get
-            {
-                return new Dictionary<string, object>
-                {
-                    { LeftColumnWidth, ON },
-                    { RightColumnWidth, OFF },
-                    { TopRowHeight, ON },
-                    { BottomRowHeight, OFF },
-                    { TextEditorColumn, 0 },
-                    { TextEditorRow, 0 },
-                    { RenderedViewColumn, 0 },
-                    { RenderedViewRow, 1 },
-                    { CurrentView, View.Horizontal }
-                };
-            }
-        }
-
-        private static IDictionary<string, object> TextEditorView
-        {
-            get
-            {
-                return new Dictionary<string, object>
-                {
-                    { LeftColumnWidth, ON },
-                    { RightColumnWidth, OFF },
-                    { TopRowHeight, ON },
-                    { BottomRowHeight, OFF },
-                    { TextEditorColumn, 0 },
-                    { TextEditorRow, 0 },
-                    { RenderedViewColumn, 1 },
-                    { RenderedViewRow, 0 },
-                    { CurrentView, View.TextEditor }
-                };
-            }
-        }
-
-        private static IDictionary<string, object> RenderedViewView
-        {
-            get
-            {
-                return new Dictionary<string, object>
-                {
-                    { LeftColumnWidth, OFF },
-                    { RightColumnWidth, ON },
-                    { TopRowHeight, ON },
-                    { BottomRowHeight, OFF },
-                    { TextEditorColumn, 0 },
-                    { TextEditorRow, 0 },
-                    { RenderedViewColumn, 1 },
-                    { RenderedViewRow, 0 },
-                    { CurrentView, View.RenderedView }
-                };
             }
         }
     }
