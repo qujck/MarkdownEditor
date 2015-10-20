364c5b82e1c82bc78b96b60d90cba340e8792cc4
Peter Parker <social@qujck.com>
2015-10-20 15:57:24 +0100

    Included untracked file

---
+++ Qujck.MarkdownEditor.Tests.Integration/DispatcherUtil.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace Qujck.MarkdownEditor.Tests.Integration
+{
+    public static class DispatcherUtil
+    {
+        [SecurityPermissionAttribute(SecurityAction.Demand, Flags = SecurityPermissionFlag.UnmanagedCode)]
+        public static void DoEvents()
+        {
+            DispatcherFrame frame = new DispatcherFrame();
+            Dispatcher.CurrentDispatcher.BeginInvoke(DispatcherPriority.Background,
+                new DispatcherOperationCallback(ExitFrame), frame);
+            Dispatcher.PushFrame(frame);
+        }
+
+        private static object ExitFrame(object frame)
+        {
+            ((DispatcherFrame)frame).Continue = false;
+            return null;
+        }
+    }
+}
