49d0449a1947bf1b4192ef932873d828dfa8417b
Peter Parker <social@qujck.com>
2015-10-23 08:24:05 +0100

    Addedd missing file

---
+++ Qujck.MarkdownEditor/Behaviours/AvalonEditTextChangedUpdateModelBehaviour.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interactivity;
+using Qujck.MarkdownEditor.ViewModel;
+
+namespace Qujck.MarkdownEditor.Behaviours
+{
+    public sealed class AvalonEditTextChangedUpdateModelBehaviour : Behavior<DocumentView>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            base.AssociatedObject.Loaded += AssociatedObject_Loaded;
+            base.AssociatedObject.Unloaded += AssociatedObject_Unloaded;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            base.AssociatedObject.Loaded -= AssociatedObject_Loaded;
+            base.AssociatedObject.Unloaded -= AssociatedObject_Unloaded;
+        }
+
+        private void AssociatedObject_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.AssociatedObject.TextEditor.TextChanged += TextEditor_TextChanged;
+        }
+
+        private void AssociatedObject_Unloaded(object sender, RoutedEventArgs e)
+        {
+            this.AssociatedObject.TextEditor.TextChanged -= TextEditor_TextChanged;
+        }
+
+        private void TextEditor_TextChanged(object sender, EventArgs e)
+        {
+            var viewModel = this.AssociatedObject.DataContext as DocumentViewModel;
+            viewModel[Constants.DocumentViewModel.CurrentText] = this.AssociatedObject.TextEditor.Text;
+        }
+    }
+}
