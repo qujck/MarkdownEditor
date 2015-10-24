08ae36b2a096101a7e50c77ddd38e13a22ec1ea7
Peter Parker <social@qujck.com>
2015-10-24 23:39:17 +0100

    missing file

---
+++ Qujck.MarkdownEditor.Tests.Integration/StubPrefixedResourcesProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Qujck.MarkdownEditor.Requests;
+
+namespace Qujck.MarkdownEditor.Tests.Integration
+{
+    internal class StubPrefixedResourcesProvider :
+        IStringRequestHandler<Strings.PrefixedResources>
+    {
+        private readonly string response;
+
+        public StubPrefixedResourcesProvider(string response)
+        {
+            this.response = response;
+        }
+
+        public string Execute(Strings.PrefixedResources query)
+        {
+            return this.response;
+        }
+    }
+}
